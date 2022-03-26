895df8bbc1328ec04161eb4c7e7d337ef32977a5
BrettREvans <Brett704@revature.net>
2022-03-25 20:00:20 -0700

    User Class Expanded to work in this project

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -1,15 +1,42 @@
 package Model;
 
 public class User {
+    private int userId = 0;
     private String userName = "";
     private String userPassword = "";
+    private String email = "";
+    private String firstName = "";
+    private String lastName = "";
+    private boolean isActive = false;
+    private String role = "";
 
+    //Sign in Constructor
     public User(String username, String password) {
 
         this.userName = username;
         this.userPassword = password;
 
     }
+
+    public User(int userId, String userName, String userPassword, String email, String firstName, String lastName, boolean isActive, String role) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userPassword = userPassword;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.isActive = isActive;
+        this.role = role;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
     public String getUserName() {
         return userName;
     }
@@ -18,12 +45,52 @@ public class User {
         this.userName = userName;
     }
 
-    public String getPassword() {
+    public String getUserPassword() {
         return userPassword;
     }
 
-    public void setPassword(String password) {
-        this.userPassword = password;
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 
     @Override
