34752629bbff98b13ea700d3723c384e37627653
BrettREvans <Brett704@revature.net>
2022-04-06 02:28:15 -0700

    rush 2 | servlet tweaks

--- src/main/java/com/revature/Controller/AdminHomeController.java
+++ src/main/java/com/revature/Controller/AdminHomeController.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
-@WebServlet("/newUsers")
+@WebServlet("/admin")
 public class AdminHomeController extends HttpServlet {
 
     private final AdminServices adminServices = new AdminServices();
@@ -51,22 +51,5 @@ public class AdminHomeController extends HttpServlet {
         resp.getOutputStream().println(json);
     }
 
-    @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPut(req, resp);
 
-        String request = req.getReader().lines().collect(Collectors.joining());
-        System.out.println(request);
-
-        int activeState = Integer.parseInt(request.split(",")[0].split("=")[1].substring(1).split("\"")[0]);
-        int userId = Integer.parseInt(request.split(",")[1].split("=")[1].substring(1).split("\"")[0]);
-        int adminId = Integer.parseInt(request.split(",")[2].split("=")[1].substring(1).split("\"")[0]);
-
-        System.out.println(activeState);
-        System.out.println(userId);
-        System.out.println(adminId);
-
-        adminServices.updateActiveState(activeState, userId, adminId);
-
-    }
 }
--- src/main/java/com/revature/Controller/ManagerUpdateController.java
+++ src/main/java/com/revature/Controller/ManagerUpdateController.java
@@ -2,10 +2,8 @@ package com.revature.Controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.Model.Enums.ReimbursementStatus;
-import com.revature.Model.Reimbursement;
 import com.revature.Service.FinancialManagerServices;
 import com.revature.Tools.Log;
-import jdk.nashorn.internal.runtime.ECMAException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,28 +13,34 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.stream.Collectors;
 
-@WebServlet("/manager")
-public class ManagerController extends HttpServlet {
+@WebServlet("/managerUpdate")
+public class ManagerUpdateController extends HttpServlet {
     private FinancialManagerServices managerServices = new FinancialManagerServices();
     private ObjectMapper mapper = new ObjectMapper();
-
-    // get all active requests
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String json = mapper.writeValueAsString(managerServices.getAllPending());
-        resp.setStatus(200);
-        resp.setContentType("application/json");
-        resp.getOutputStream().println(json);
-    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String reqString = req.getReader().lines().collect(Collectors.joining());
-        String json = mapper.writeValueAsString(managerServices.getManagerHistory(Integer.parseInt(reqString.split(",")[0].split("\"")[3])));
+        System.out.println(reqString);
 
-        resp.setStatus(200);
-        resp.setContentType("application/json");
-        resp.getOutputStream().println(json);
+        try {
+            int reimbId = Integer.parseInt(reqString.split(",")[0].split("\"")[3]);
+            String timeResolved = reqString.split(",")[1].split("\"")[3];
+            int managerId = Integer.parseInt(reqString.split(",")[2].split("\"")[3]);
+            ReimbursementStatus statusEnum = ReimbursementStatus.valueOf(reqString.split(",")[3].split("\"")[3]);
+            System.out.println(reqString.split(",")[3].split("\"")[3]);
+            managerServices.resolveReimbursement(reimbId,timeResolved, managerId, statusEnum);
+
+        } catch (Exception e) {
+            Log.setupLogger();
+            Log.logMessage("warn", "prob" +e.getMessage());
+
+        }
+
+
     }
-    
 }
+
+
+
+
